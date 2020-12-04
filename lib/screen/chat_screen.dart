5467186fa0961c7600d27f2c318032561d429e0c
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Pro.local>
2020-12-04 20:04:19 +0300

    326. Listening to Chat Messages

--- lib/screen/chat_screen.dart
+++ lib/screen/chat_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:maxi_app6_2_shop_app/widgets/chat/messages.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
@@ -38,25 +39,12 @@ class ChatScreen extends StatelessWidget {
           )
         ],
       ),
-      body: StreamBuilder(
-        stream: FirebaseFirestore.instance
-            .collection("chats/mOuuVqS7tDSSNeo8NoIX/messages")
-            .snapshots(),
-        builder: (ctx, streamSnapshot) {
-          if (streamSnapshot.connectionState == ConnectionState.waiting) {
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-
-          final documents = streamSnapshot.data.docs;
-          return ListView.builder(
-              itemCount: documents.length,
-              itemBuilder: (ctx, index) => Container(
-                    padding: EdgeInsets.all(8),
-                    child: Text(documents[index]["text"]),
-                  ));
-        },
+      body: Container(
+        child: Column(
+          children: [
+            Expanded(child: Messages()),
+          ],
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
