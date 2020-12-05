b088761bf35d1a7361efef6a0cfaa4fcc89c9200
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Pro.local>
2020-12-05 19:16:15 +0300

    334. Preparing File Upload & Validating Images

--- lib/widgets/pickers/user_image_picker.dart
+++ lib/widgets/pickers/user_image_picker.dart
@@ -4,6 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
 class UserImagePicker extends StatefulWidget {
+  UserImagePicker(this.imagePickFn);
+  final void Function(File pickedImage) imagePickFn;
+
   @override
   _UserImagePickerState createState() => _UserImagePickerState();
 }
@@ -18,6 +21,7 @@ class _UserImagePickerState extends State<UserImagePicker> {
     setState(() {
       _pickedImage = File(pickedImageFile.path);
     });
+    widget.imagePickFn(File(pickedImageFile.path));
   }
 
   @override
