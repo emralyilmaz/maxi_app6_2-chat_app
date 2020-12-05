defe417fdfd1c5e7e9aba5b7394ca24bbe2f23cf
Emral Yılmaz <emralyilmaz@Emrals-MacBook-Pro.local>
2020-12-05 13:27:34 +0300

    329. Improving the Chat Bubbles

--- lib/widgets/chat/messages.dart
+++ lib/widgets/chat/messages.dart
@@ -25,6 +25,8 @@ class Messages extends StatelessWidget {
             itemBuilder: (ctx, index) => MessageBubble(
               chatDocs[index].data()['text'],
               chatDocs[index].data()['userId'] == user.uid,
+              key: ValueKey(chatDocs[index]
+                  .id), // daha inceden id yerine documentID kullanılıyordu.
             ),
           );
         });
